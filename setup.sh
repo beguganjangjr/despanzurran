f66fae4d39fbc7548b11a0418a9deda22e62b6fe
beguganjangjr <64639270+beguganjangjr@users.noreply.github.com>
2021-09-05 15:57:26 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,8 +13,8 @@ export PATH=$PWD:$PATH
 chmod 777 /app/accounts
 
 # Install aria2c static binary
-wget -q https://github.com/P3TERX/Aria2-Pro-Core/releases/download/1.35.0_2021.02.19/aria2-1.35.0-static-linux-amd64.tar.gz
-tar xf aria2-1.35.0-static-linux-amd64.tar.gz
+wget -q https://github.com/P3TERX/Aria2-Pro-Core/releases/download/1.36.0_2021.08.22/aria2-1.36.0-static-linux-amd64.tar.gz
+tar xf aria2-1.36.0-static-linux-amd64.tar.gz
 export PATH=$PWD:$PATH
 
 # Create download folder

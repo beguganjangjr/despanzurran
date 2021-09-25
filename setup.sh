3793ed586bb4312eac673338a91243feccb0e5a4
beguganjangjr <64639270+beguganjangjr@users.noreply.github.com>
2021-09-25 11:46:51 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,6 +30,9 @@ PORTING=$PORT
 echo "$(curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt)" > trackers.txt
 echo "$(curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all_ip.txt)" >> trackers.txt
 echo "$(curl -Ns https://newtrackon.com/api/stable)" >> trackers.txt
+echo "$(curl -Ns https://newtrackon.com/api/live)" >> trackers.txt
+echo "$(curl -Ns https://cdn.jsdelivr.net/gh/ngosang/trackerslist@master/trackers_all.txt)" >> trackers.txt
+
 tmp=$(sort trackers.txt | uniq) && echo "$tmp" > trackers.txt
 sed -i '/^$/d' trackers.txt
 sed -i ":a;N;s/\n/,/g;ta" trackers.txt

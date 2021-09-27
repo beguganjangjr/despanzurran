b37cfa77357dc1283d6de6ace88901544b0d5244
beguganjangjr <64639270+beguganjangjr@users.noreply.github.com>
2021-09-27 10:59:14 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -44,7 +44,7 @@ fi
 echo "adding trackers and set listen-port=$(( PORT - 1 ))-$(( PORT + 1 )) and dht-listen-port=6881-6999,$PORT"
 
 echo "bt-tracker=$tracker_list" >> aria2c.conf
-echo "listen-port=$(( PORT - 1 ))-$(( PORT + 1 ))" >> aria2c.conf
+echo "listen-port=6881-6999,$(( PORT - 1 ))-$(( PORT + 1 ))" >> aria2c.conf
 #echo "dht-listen-port=6881-6999" >> aria2c.conf
 
 echo $PATH > PATH

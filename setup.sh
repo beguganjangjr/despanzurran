75a6cdc67257ab2a53969d517c1f0bf89a343ef9
beguganjangjr <64639270+beguganjangjr@users.noreply.github.com>
2021-10-23 13:29:38 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ export PATH=$PWD:$PATH
 chmod 777 /app/gclone
 
 #Inject Rclone config
-curl -o accounts.zip "${SAURL}"
+curl -so accounts.zip "${SAURL}"
 unzip -q accounts.zip
 export PATH=$PWD:$PATH
 chmod 777 /app/accounts

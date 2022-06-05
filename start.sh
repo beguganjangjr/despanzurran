ba89c0f5202310449ea58462ce69c22754dcc031
beguganjangjr <64639270+beguganjangjr@users.noreply.github.com>
2022-06-05 14:52:35 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+wget -q https://github.com/q3aql/aria2-static-builds/releases/download/v1.35.0/aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2
+tar xf aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2
+export PATH=$PWD/aria2-1.35.0-linux-gnu-64bit-build1:$PATH
 if [[ -n $RCLONE_CONFIG && -n $RCLONE_DESTINATION ]]; then
 	echo "Rclone config detected"
 	echo -e "$RCLONE_CONFIG" > rclone.conf

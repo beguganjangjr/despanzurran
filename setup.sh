5eac5232ab3a717d2a9b8adea147e010945233f9
beguganjangjr <64639270+beguganjangjr@users.noreply.github.com>
2022-06-05 14:52:15 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,9 +14,9 @@ export PATH=$PWD/rar:$PATH
 unrar -p"${SA_SECRET}" e accounts.rar /app/accounts/
 
 # Install aria2c static binary
-wget -q https://github.com/q3aql/aria2-static-builds/releases/download/v1.35.0/aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2
-tar xf aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2
-export PATH=$PWD/aria2-1.35.0-linux-gnu-64bit-build1:$PATH
+#wget -q https://github.com/q3aql/aria2-static-builds/releases/download/v1.35.0/aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2
+#tar xf aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2
+#export PATH=$PWD/aria2-1.35.0-linux-gnu-64bit-build1:$PATH
 
 # Create download folder
 mkdir -p downloads
